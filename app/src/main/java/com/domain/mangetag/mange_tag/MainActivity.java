e44ff3990d22e2458a3be7923accecd83d68e2d3
zhaozihao <18521706275@163.com>
2017-06-20 15:07:12 +0800

    tag提交

--- app/src/main/java/com/domain/mangetag/mange_tag/MainActivity.java
+++ app/src/main/java/com/domain/mangetag/mange_tag/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("zzh","main");
         Log.d("zzh","2");
+        Log.d("zzh","tag");
     }
 }
